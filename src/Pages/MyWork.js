1de630e055c9ca2634ddf81d11c1998ed54eccce
Christina Bui <uyen.buitie@gmail.com>
2024-09-29 00:50:03 -0500

    Tweak My Work

--- src/Pages/MyWork.js
+++ src/Pages/MyWork.js
@@ -5,7 +5,8 @@ import Faces from '../Components/Faces';
 import Card from '../Components/Card';
 function Work() {
   return (
-    <section>
+    <>
+       <section>
         <SubPage
           title = "My Work"
           previousPage= "Homepage"
@@ -13,20 +14,22 @@ function Work() {
           previousPageURL="/"
         />
        
-       <h1 className='text-center secondary-blue-color'>Case Studies</h1>
-       <div className='horizontal-flex wrap' id='case-studies'>
-         <Card
-          type = "Big Vertical"
-          imgSrc = "RageRoomMobile"
-          companyName= "The Rage Room"
-          description= "In this case study, I will detail the iterative redesign of a rage room booking experience that aimed to enhance user engagement and streamline the booking process."
-          companyType= "Website UX Design"
-          linkText= "View Case Study"
-          linkHref="/Work/Case-Studies/Enhancing-UX-Booking"
-         />
-       </div>
+     
+        <h1 className='text-center secondary-blue-color'>Case Studies</h1>
+        <div className='horizontal-flex wrap' id='case-studies'>
+          <Card
+            type = "Big Vertical"
+            imgSrc = "RageRoomMobile"
+            companyName= "The Rage Room"
+            description= "In this case study, I will detail the iterative redesign of a rage room booking experience that aimed to enhance user engagement and streamline the booking process."
+            companyType= "Website UX Design"
+            linkText= "View Case Study"
+            linkHref="/Work/Case-Studies/Enhancing-UX-Booking"
+          />
+        </div>
+       </section>
 
-  </section>
+  </>
   );
 }
 
