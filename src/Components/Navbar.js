e3a9cb9c6d2d73804880e33d6c81580d00451ef5
Christina Bui <uyen.buitie@gmail.com>
2024-09-30 20:48:36 -0500

    Navbar Tweak

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Styles/Navbar.css';
 import logo from '../Assets/Logo.png';
@@ -20,10 +20,38 @@ const Navbar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
+  // State to track whether user is scrolling
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let isScrollingTimeout;
+
+    const handleScroll = () => {
+      // User is scrolling, so set isScrolling to true
+      setIsScrolling(true);
+
+      // Clear the previous timeout if it exists
+      if (isScrollingTimeout) clearTimeout(isScrollingTimeout);
+
+      // Set a timeout to remove the background after scrolling stops
+      isScrollingTimeout = setTimeout(() => {
+        setIsScrolling(false);
+      }, 200); // Adjust the timeout (in ms) as necessary
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(isScrollingTimeout);
+    };
+  }, []);
+
   return (
     <>
       {/* Desktop Navbar */}
-      <div className={`navbar-desktop ${currentPath === '/' ? 'homepage-navbar' : 'otherpage-navbar'}`}>
+      <div className={`navbar-desktop ${currentPath === '/' ? 'homepage-navbar' : 'otherpage-navbar'} ${isScrolling ? 'scrolled' : ''}`}>
         <div className="navbar-container">
           <div className="navbar-item">
             <Link to="/" className="navbar-link">
@@ -31,34 +59,35 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="navbar-item main">
-            <div> <Link 
+            <div>
+              <Link 
                 to="/About" 
                 className={`navbar-link ${currentPath === '/About' ? 'active' : ''}`}
               >
                 About Me
-              </Link></div>
-              
-            <div>  <Link 
+              </Link>
+            </div>
+            <div>
+              <Link 
                 to="/Work" 
                 className={`navbar-link ${currentPath === '/Work' ? 'active' : ''}`}
               >
                 My Work
-              </Link></div>
-
-            <div>
-                <Link 
-                    to="/Contact" 
-                    className={`navbar-link ${currentPath === '/Contact' ? 'active' : ''}`}
-                >
-                    <Button
-                    type="button"
-                    className={currentPath === '/Contact' ? 'button--disabled' : 'button--blue'}
-                    >
-                    Contact Me
-                    </Button>
-                </Link>
+              </Link>
             </div>
-
+            <div>
+              <Link 
+                to="/Contact" 
+                className={`navbar-link ${currentPath === '/Contact' ? 'active' : ''}`}
+              >
+                <Button
+                  type="button"
+                  className={currentPath === '/Contact' ? 'button--disabled' : 'button--blue'}
+                >
+                  Contact Me
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -73,7 +102,6 @@ const Navbar = () => {
             <p className="nav-txt">Home</p>
           </Link>
         </div>
-
         <div className="navbar-item">
           <Link to="/Work" className="navbar-link">
             <div className="icon-container">
@@ -82,7 +110,6 @@ const Navbar = () => {
             <p className="nav-txt">Work</p>
           </Link>
         </div>
-
         <div className="navbar-item">
           <Link to="/About" className="navbar-link">
             <div className="icon-container">
@@ -91,7 +118,6 @@ const Navbar = () => {
             <p className="nav-txt">About</p>
           </Link>
         </div>
-
         <div className="navbar-item">
           <Link to="/Contact" className="navbar-link">
             <div className="icon-container">
